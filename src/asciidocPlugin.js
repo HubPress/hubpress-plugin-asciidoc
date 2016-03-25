48ed77f2ad5db76a66619c9740c59ad69cf7eb45
Anthonny <anthonny.querouil@gmail.com>
2016-03-25 12:22:00 +0100

    fix(post): fix the published date of the post and image

--- src/asciidocPlugin.js
+++ src/asciidocPlugin.js
@@ -1,27 +1,27 @@
-import asciiDoctorLib from 'asciidoctor.js'
-import _ from 'lodash'
-import moment from 'moment'
-import slugify from 'hubpress-core-slugify'//'./utils/slugify'
+import asciiDoctorLib from 'asciidoctor.js';
+import _ from 'lodash';
+import moment from 'moment';
+import slugify from 'hubpress-core-slugify';//'./utils/slugify'
 
 const asciidoctor = asciiDoctorLib();
 const opal = asciidoctor.Opal;
 const processor = asciidoctor.Asciidoctor();
-const options = opal.hash({
-  doctype: 'article',
-  backend: 'html5',
-  safe: 'safe',
-  attributes: ['showtitle!','imagesdir=/images', 'icons=font']
-});
 
 function splitMore(asciidocContent) {
   let parts = asciidocContent.split('pass::[more]');
   return {
     excerpt: parts[0],
     full: parts.join('')
-  }
+  };
 }
 
-function convert (asciidocContent) {
+function convert (opts, asciidocContent) {
+  const options = opal.hash({
+    doctype: 'article',
+    backend: 'html5',
+    safe: 'safe',
+    attributes: ['showtitle!',`imagesdir=${opts.data.config.urls.site}/images`, 'icons=font']
+  });
   let parts = splitMore(asciidocContent);
   let excerpt = processor.$load(parts.excerpt, options);
   let doc = processor.$load(parts.full, options);
@@ -29,16 +29,23 @@ function convert (asciidocContent) {
     attributes: _.pick(doc.attributes, ['smap']),
     excerpt: excerpt.$convert(),
     html: doc.$convert()
-  }
+  };
   return value;
+}
+
+function extractTags (attributes) {
+  const tagAttribute = 'hp-tags';
+  return attributes.smap[tagAttribute] && attributes.smap[tagAttribute].split(',');
 }
 
 export function asciidocPlugin (hubpress) {
 
   hubpress.on('requestRenderingDocuments', (opts) => {
+    console.info('Asciidoc Plugin - requestRenderingDocuments');
+    console.log('requestRenderingDocuments', opts);
 
     const posts = (opts.data.documents.posts || []).map((post) => {
-      const _post = Object.assign({}, post, convert(post.content), {
+      const _post = Object.assign({}, post, convert(opts, post.content), {
         content: post.content
       });
 
@@ -47,30 +54,30 @@ export function asciidocPlugin (hubpress) {
 
       _post.title = original.title = original.attributes.smap['doctitle'] ;
       _post.image = original.image = original.attributes.smap['hp-image'] ;
-      _post.tags = original.tags = original.attributes.smap['hp-tags'] && original.attributes.smap['hp-tags'].split(',') ;
+      _post.tags = original.tags = extractTags(original.attributes);
       _post.url = original.url = opts.data.config.urls.getPostUrl(original.name);
 
       let _postToSave = Object.assign({}, _post, {original: original});
       _postToSave.original.published_at = _postToSave.published_at = original.name.split('-').slice(0,3).join('-');
 
       return _postToSave;
-    })
+    });
 
     const mergeDocuments = Object.assign({}, opts.data.documents, {posts});
     const data = Object.assign({}, opts.data, {documents: mergeDocuments});
     return Object.assign({}, opts, {data});
-  })
+  });
 
   hubpress.on('requestRenderingPost', (opts) => {
+    console.info('Asciidoc Plugin - requestRenderingPost');
+    console.log('requestRenderingPost', opts);
 
-    let refreshedPost = convert(opts.data.post.content);
-     //Object.assign(opts.data, {document: convertedContent});
+    let refreshedPost = convert(opts, opts.data.post.content);
     refreshedPost._id = opts.data.post._id;
-    console.log('refreshedPost', refreshedPost);
     refreshedPost.title = refreshedPost.attributes.smap['doctitle'];
     refreshedPost.image = refreshedPost.attributes.smap['hp-image'] ;
-    refreshedPost.tags = refreshedPost.attributes.smap['hp-tags'] && refreshedPost.attributes.smap['hp-tags'].split(',');
-    refreshedPost.published_at = refreshedPost.attributes.smap['published_at'] || refreshedPost.published_at || moment().format('YYYY-MM-DD');
+    refreshedPost.tags = extractTags(refreshedPost.attributes);
+    refreshedPost.published_at = refreshedPost.attributes.smap['published_at'] || opts.data.post.published_at || moment().format('YYYY-MM-DD');
     let altTitle = refreshedPost.attributes.smap['hp-alt-title'];
     refreshedPost.name = slugify(refreshedPost.published_at + '-' + (altTitle || refreshedPost.title)) +'.adoc';
     refreshedPost.url = opts.state.application.config.urls.getPostUrl(refreshedPost.name);
@@ -78,6 +85,6 @@ export function asciidocPlugin (hubpress) {
     const mergedPost = Object.assign({}, opts.data.post, refreshedPost);
     const data = Object.assign({}, opts.data, {post: mergedPost});
     return Object.assign({}, opts, {data});
-  })
+  });
 
 }
---
+++ test/asciidocPlugin.js
@@ -0,0 +1,11 @@
+/*eslint-env node, mocha */
+import chai from 'chai';
+//import asciidocPlugin from '../src/asciidocPlugin';
+
+chai.should();
+
+describe('test', () => {
+  it('should pass', () => {
+    '1'.should.equal('1');
+  });
+});
